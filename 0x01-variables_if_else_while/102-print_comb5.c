e46f44e1a09985fb83d90a82312e1e3d9df41ee1
Tolupkia <tolupkia5@gmail.com>
2022-09-12 11:36:29 -0700

    initial commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 			c = j / 10;/*doubles snum*/
 			d = j % 10;/*singles snum*/
 
-			if (a < c ||(a == c && b > d))
+			if (a < c || (a == c && b > d))
 			{
 				putchar(a + '0');
 				putchar(b + '0');
