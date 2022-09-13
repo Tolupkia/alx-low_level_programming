866cb006115f8b0f55e38f3572e0fd729b94104d
Tolupkia <tolupkia5@gmail.com>
2022-09-13 16:46:21 -0700

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,14 @@
 
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
