ea2ae7926c10976a69b2894f7935bf4d0ea40951
Tolupkia <tolupkia5@gmail.com>
2022-09-13 18:52:27 -0700

    initial commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,18 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
-		n *= -1;
-
-	_putchar('0' + (n % 10));
-
-	return (n % 10);
+	{
+		last_digit = -1 * (n % 10);
+		_putchar(last_digit + 48);
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + 48);
+		return (last_digit);
+	}
 }
