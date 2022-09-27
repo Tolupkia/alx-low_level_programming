6ab58786021b928bf2e3d08ff8a3035ec704dde5
Tolupkia <tolupkia5@gmail.com>
2022-09-26 18:48:39 -0700

    initial commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,26 +11,31 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, match;
+	int i, j, x;
 
-	if (*needle == '\0')
+	if (needle[0] == '\0')
 		return (haystack);
-	for (i = 0; haystack[i] != '\0'; i++)
+
+	while (haystack[i] != '\0') /* iterate through haystack*/
 	{
-		if (haystack[i] == *needle)
+		/* if a byte matches first char of needle */
+		/* iterate through needle until match end */
+		if (haystack[i] == needle[0])
 		{
-			for (j = 0; needle[j] != '\0'; j++)
+			x = i. j = 0;
+			while (needle[j] != '\0.)
 			{
-				if (needle[j] != haystack[i + j])
-				{
-					match = 0;
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
 					break;
-				}
-				match = 1;
+			} /* if matched throughout, return haystack */
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
 			}
-			if (match)
-				return (haystack + 1);
 		}
+		i++;
 	}
-	return (NULL);
+	return (NULL); /* no match */
 }
