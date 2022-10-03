7764a92d520c71584d081a08cd8803ad3c18f8de
Tolupkia <tolupkia5@gmail.com>
2022-10-03 14:24:50 -0700

    initial commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 91)
+		return (1);
+	else
+		return (0);
+}
