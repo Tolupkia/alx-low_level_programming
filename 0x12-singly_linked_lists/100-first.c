0be9e3142cd571372cbe82d8995367cfd8b3aeb7
Tolupkia <tolupkia5@gmail.com>
2022-10-21 17:49:30 -0700

    function that print before main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-void myStartupFun(void)__attribute__ ((constructor));
 
 /**
- * myStartupFun - function that runs before main
+ * bmain - function that runs before main
  */
 
-void myStartupFun(void)
+void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(", \nI bore my house upon my back!\n");
 }
